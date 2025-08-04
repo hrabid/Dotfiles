f459f33396256e337268ec4b1c33c53ef20915be
Habibur Rahman Abid <hrahmanabid@proton.me>
2025-08-04 20:30:33 +0600

    added a script & modified env var file

--- shell/.envvar.sh
+++ shell/.envvar.sh
@@ -2,7 +2,7 @@
 GHREPO=$HOME/Repos/gh/
 GHHR=$HOME/Repos/gh/hrabid/
 GHHRA=$HOME/Repos/gh/hrahmanabid/
-GHDOT=$HOME/Repos/gh/hrahmanabid/Dotfiles/
+GHDOT=$HOME/Dotfiles/
 GHO=$HOME/Repos/gh/others/
 
 # PATH
